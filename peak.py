59bf0cb6c0620be17062b038bddbe07e95ef2345
Ubuntu <ubuntu@ip-172-31-17-77.ec2.internal>
2024-10-10 19:41:37 +0000

    Adding links

--- peak.py
+++ peak.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', mountain="Everest")
+    mountains = ['Everest', 'K2', 'Kilimanjaro']
+    return render_template('index.html', mountain= mountains)
 
+@app.route('/mountain/<mt>')
+def mountain(mt):
+    return "This is " + str(mt)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
